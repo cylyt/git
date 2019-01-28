0ba9341b455d3dd52316143d20bd995a817021e2
AL <lvyt025@126.com>
2019-01-28 15:46:05 +0800

    20190128

--- automate/excel/multiplication.py
+++ automate/excel/multiplication.py
@@ -1,3 +1,4 @@
+from openpyxl.styles import Font
 import openpyxl
 import os
 
@@ -6,9 +7,12 @@ ws=wb.create_sheet('table')
 
 for i in range (1,7):
     for j in range(1,7):
+        if i==1:
+            ws.cell(row=1,column=j+1).value=i*j
+            ws.cell(row=1,column=j+1).font=Font(bold=True)
         if j==1:
-            ws.cell(row=i,column=j).value=i*j
-            ws.cell(row=i,column=j).font=Font(bold=True)
+            ws.cell(row=i+1,column=j).value=i*j
+            ws.cell(row=i+1,column=j).font=Font(bold=True)
         ws.cell(row=i+1,column=j+1).value=i*j
 
 wb.save('multiplication.xlsx')
