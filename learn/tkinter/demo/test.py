b3f2bc954a5a4e5714bc6999636b4ff89a8633b8
AL <lvyt025@126.com>
2019-02-12 16:58:49 +0800

    20190212

---
+++ learn/tkinter/demo/test.py
@@ -0,0 +1,5 @@
+from tkinter import *
+from tkinter import ttk
+root=Tk()
+ttk.Button(root,text="Helle World").grid
+root.manloop()
--- test.py
+++ test.py
@@ -1,2 +1,31 @@
+people={}
 
+for x in range(1,31):
+    people[x]=1
+#print(people)
+
+check=1
+i=1
+j=0
+
+while i<=31:
+    if i==31:
+        i=1
+        continue
+    elif j==15:
+        break
+    
+    if people[i]==0:
+        i+=1
+        continue
+    else:
+        if check==9:
+            people[i]=0
+            print('第%d个人下船' %i)
+            check=0
+            j+=1
+        check+=1
+        i+=1
+    
+print(people)
         
