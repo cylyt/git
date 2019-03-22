8af83f05e4ee0f7c393a1cf35b4673eefc1c8a66
AL <lvyt025@126.com>
2019-03-22 13:54:19 +0800

    20190322

--- learn/pygame/game9/game9.py
+++ learn/pygame/game9/game9.py
@@ -1,6 +1,49 @@
 import pygame
 import random
 
+class Farmer(pygame.sprite.Sprite):
+    
+    def __init__(self,screenWidth,screenHeight):
+        pygame.sprite.Sprite.__init__(self)
+        self.imgs = ['./img/farmer.png']
+        self.farmer = pygame.image.load(self.imgs[0].convert_alpha)
+        self.direction_dice = {
+                                'top': [0, (0, -1)],
+                                'righttop': [1, (1, -1)],
+                                'right': [2,(1, 0)],
+                                'rigthbottom': [3, (1, 1)],
+                                'bottom': [4, (0, 1)],
+                                'leftbottom': [5, (-1, 1)],
+                                'left': [6, (-1, 0)],
+                                'lefttop': [7, (-1, -1)]
+        }
+
+        self.direction = 'left'
+        self.farmerIdx = 0
+        self.farmerIdNum = 0
+        self.x, self.y = screenWidth/2, screenHeight/1.1
+        self.speed = 5
+        self.screenWidth, self.screenHeight = screenWidth, screenHeight
+        self.move()
+    
+    def move(self,direction = 'left'):
+        if direction != self.direction:
+            self.direction = direction
+            self.farmerIdx = 0
+        else:
+            self.farmerIdx += 1
+            self.farmerIdx = self.farmerIdx % self.farmerIdNum
+        farmerPos = self.farmerIdx *96, self.direction_dict[self.direction][0]*96 
+        self.image = self.farmer.subsurface(farmerPos,(96, 96))
+        self.rect = self.image.get_rect()
+        self.x = self.x + self.direction_dict[self.direction[1][0]]*self.speed
+        self.y = self.y + self.direction_dict[self.direction[1][1]]*self.speed
+        self.rect.left, self.rect.top = self.x, self.y
+
+        self.rect.right = self.screenWidth if self.rect.right > self.screenWidth else self.rect.right
+        self.rect
+    
+
 class Food(pygame.sprite.Sprite):
 
     def __init__(self,screenWidth,screenHeight):
