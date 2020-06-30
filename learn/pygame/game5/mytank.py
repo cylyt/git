622627d84fe6c6353b54ff457dbd973266cccce1
AL <lvyt025@126.com>
2020-06-30 14:36:33 +0800

    20200630

--- learn/pygame/game5/mytank.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from pygame.sprite import Sprite
-
-class Mytank(Sprite):
-
-    def __init__(self):
-
-        pass
-
--- learn/pygame/mysprite/my_sprite.py
+++
@@ -1,8 +0,0 @@
-#精灵类 生产动画
-import pygame
-
-class MySprite(pygame.sprite.Sprite):
-
-    def __init__(self,targe):
-        pygame.sprite.Sprite.__init__(self):
-        
