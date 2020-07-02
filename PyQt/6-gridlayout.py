025a3ad29b1035b4a3dab239eb9ecb78140d1f42
cylyt <lvyt025@126.com>
2020-07-02 22:00:14 +0800

    20200702

--- PyQt/6-gridlayout.py
+++ PyQt/6-gridlayout.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import (QWidget,QGridLayout,
-    QPushButton,QApplication)
+    QPushButton,QApplication,QLCDNumber)
 
 class Example(QWidget):
 
@@ -12,12 +12,18 @@ class Example(QWidget):
 
         grid = QGridLayout()
         self.setLayout(grid)
+        self.setGeometry(300, 300, 400, 300)
+        self.setWindowTitle('Calculator')
+
+        self.lcd = QLCDNumber()
+        grid.addWidget(self.lcd, 0, 0, 3, 0)
+        grid.setSpacing(10)
         names = ['Cls', 'Bck','', 'Close',
                 '7', '8', '9', '/',
                 '4', '5', '6', '*',
                 '1', '2', '3', '-',
                 '0', '.', '=','+']
-        positions = [(i,j) for i in range(5) for j in range(4)]
+        positions = [(i,j) for i in range(4, 9) for j in range(4, 8)]
 
         for position, name in zip(positions, names):
 
@@ -25,10 +31,19 @@ class Example(QWidget):
                 continue
             button = QPushButton(name)
             grid.addWidget(button, *position)
+            button.clicked.connect(self.Cli)
 
         self.move(300,150)
-        self.setWindowTitle('Calculator')
+        
         self.show()
+    
+    def Cli(self):
+        sender = self.sender().text()
+        ls = ['/', '.', '-', '=', '+']
+        if sender in ls:
+            self.lcd.display('A')
+        else:
+            self.lcd.display(sender)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv) # 创建应用程序对象
